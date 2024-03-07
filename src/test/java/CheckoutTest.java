4ceb7b08f774fff1b5d0dc377bb2ae880dd489a4
codewiser116 <benazir.baialieva@gmail.com>
2024-03-06 21:04:08 -0600

    first commit

---
+++ src/test/java/CheckoutTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class CheckoutTest {
+
+    @Test
+    public void testCheckout(){
+        System.out.println("checkout test");
+    }
+
+}
---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,18 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LoginTest {
+
+
+    @Test
+    public void testLogin1(){
+        System.out.println("This is test 1 from Login test");
+    }
+
+
+    @Test
+    public void testLogin2(){
+        System.out.println("This is the second test from Login test");
+    }
+
+}
